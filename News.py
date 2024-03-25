ef48fa030211bfe3671f8a770affa759b4a5dd59
ruchir404 <ruchirchitre@gmail.com>
2024-03-25 20:16:55 +0530

    commit

--- News.py
+++ News.py
@@ -1,17 +1,15 @@
-import requests 
-from RuchirApiKey import *
-api_address="http://newsapi.org/v2/top-readlines?country=us&apiKey"+api_key
+import requests
+from RuchirApiKey import api_key
+
+api_address = "http://newsapi.org/v2/top-headlines?country=us&apiKey=" + api_key
 json_data = requests.get(api_address).json()
 
-ar=[]
+ar = []
 
 def news():
     for i in range(3):
-        ar.append("Number "+ str(i+1)+", "+ json_data["articles"][i]["title"]+".")
+        ar.append("Number " + str(i+1) + ": " + json_data["articles"][i]["title"])
 
     return ar
 
-
-
-
-
+print(news())
--- Ruchir.py
+++ Ruchir.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 
-class music():
+class Music:
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path='C:\ProgramData\Microsoft\Windows\Start Menu\Programs')
+        self.driver = webdriver.Chrome(executable_path=r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs')
 
-    def play(self_, query):
+    def play(self, query):
         self.query = query
         self.driver.get(url="https://www.youtube.com/results?search_query=" + query)
-        video= self.driver.find_element_by_xpath('//*[@id="dismissable"]')
+        video = self.driver.find_element_by_xpath('//*[@id="dismissable"]')
         video.click()
 
