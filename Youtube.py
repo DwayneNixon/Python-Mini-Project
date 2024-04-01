51622b16791687df90386509b9408e455520d9fa
Dwayne  Nixon <dwaynenixon2004@gmail.com>
2024-04-01 23:12:16 +0530

    Small Edit in Youtube.py file

--- Youtube.py
+++ Youtube.py
@@ -14,13 +14,7 @@ class YouTubeSearchAndPlay():
         video = self.driver.find_element(By.XPATH, '//*[@id="meta"]')
         video.click()
 
-        #time.sleep(5)  # Wait for 5 seconds for search results to load
-        # Click on the first video that appears in the search results
-        #first_video = self.driver.find_element(By.XPATH, '//*[@id="contents"]/ytd-video-renderer[1]')
-        #first_video.click()
-
-        # Let the video play for some time
-        time.sleep(60)  # Adjust the time as needed
+        time.sleep(60)
 
         self.driver.quit()
 
